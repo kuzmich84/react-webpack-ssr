3056eecfdada27211d492baf1e8e4b13ca5f542d
dmitry <kudim@inbox.ru>
2021-09-26 06:54:57 +0300

    create hot module replacement

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,9 +1,13 @@
+import {hot} from "react-hot-loader/root"
 import React from "react";
 
-export function Header(){
+
+function HeaderComponent() {
     return (
         <header>
             <h1>Hello React SSR</h1>
         </header>
     )
 }
+
+export const Header = hot(HeaderComponent);
