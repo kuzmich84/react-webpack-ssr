177ee6fe26521df945d4166ea0fbc0d06334cec4
dmitry.kuchukov <kudim84@gmail.com>
2021-11-13 19:43:18 +0300

    create token action

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,6 @@
 import {ActionCreator, AnyAction, Reducer} from "redux";
+const UPDATE_COMMENT = 'UPDATE_COMMENT';
+const SET_TOKEN = 'SET_TOKEN';
 
 export type RootState = {
     commentText: string
@@ -10,14 +12,21 @@ const initialState = {
 
 export const updateComment: ActionCreator<AnyAction> = (text) => {
     return {
-        type: 'UPDATE_COMMENT',
+        type: UPDATE_COMMENT,
         text
+    }
+}
+
+export const setToken: ActionCreator<AnyAction> = (token) => {
+    return {
+        type: SET_TOKEN,
+        token
     }
 }
 
 export const rootReducer: Reducer<RootState> = (state = initialState, action) => {
     switch (action.type) {
-        case 'UPDATE_COMMENT':
+        case UPDATE_COMMENT:
             return {
                 ...state,
                 commentText: action.text
